a7aec713638e2fd9d39c5ba86f14d502d8bb69ce
dkovalenko0 <kovalenkodima715@gmail.com>
2020-05-01 18:02:05 +0300

    create header block

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
 // Your main script
 
-const index = 'This is a placeholder!';
+let figure = $(".video").hover( hoverVideo, hideVideo );
+
+function hoverVideo(e) {  
+    $('video', this).get(0).play(); 
+}
+
+function hideVideo(e) {
+    $('video', this).get(0).pause(); 
+}
