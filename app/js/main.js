d0f72e6d86901a373ff534114b96813426ff9b8f
dkovalenko0 <kovalenkodima715@gmail.com>
2020-05-03 00:04:03 +0300

    add mask to input, add scroll and animation to arrow, add change color to languages

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,32 @@
 // Your main script
+$(function(){
+	let figure = $(".video").hover( hoverVideo, hideVideo );
 
-let figure = $(".video").hover( hoverVideo, hideVideo );
+	function hoverVideo(e) {  
+	    $('video', this).get(0).play(); 
+	}
 
-function hoverVideo(e) {  
-    $('video', this).get(0).play(); 
-}
+	function hideVideo(e) {
+	    $('video', this).get(0).pause(); 
+	}
 
-function hideVideo(e) {
-    $('video', this).get(0).pause(); 
-}
+  $(".phone").mask("+38 (999) 999-99-99");
+
+  $(".dropdown-arrow").click(function (){
+        $('html, body').animate({
+             scrollTop: $(".about").offset().top
+         }, .5);
+   });
+
+  $(".rus").click(function() {
+  	$(".rus").addClass('active').removeClass('disable')
+  	$(".en").addClass('disable').removeClass('active')
+  })
+
+  $(".en").click(function() {
+  	$(".en").addClass('active').removeClass('disable')
+  	$(".rus").addClass('disable').removeClass('active')
+  })
+
+
+});
