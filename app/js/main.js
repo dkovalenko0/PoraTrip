bdfc29b52b08b99e52b057d08a1b82501c6d9c3f
dkovalenko0 <dimakovalenko715@gmail.com>
2020-06-29 19:33:20 +0300

    add burger menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,32 +1,38 @@
 // Your main script
-$(function(){
-	let figure = $(".video").hover( hoverVideo, hideVideo );
+$(function () {
+  let figure = $(".video").hover(hoverVideo, hideVideo);
 
-	function hoverVideo(e) {  
-	    $('video', this).get(0).play(); 
-	}
+  function hoverVideo(e) {
+    $("video", this).get(0).play();
+  }
 
-	function hideVideo(e) {
-	    $('video', this).get(0).pause(); 
-	}
+  function hideVideo(e) {
+    $("video", this).get(0).pause();
+  }
 
   $(".phone").mask("+38 (999) 999-99-99");
 
-  $(".dropdown-arrow").click(function (){
-        $('html, body').animate({
-             scrollTop: $(".about").offset().top
-         }, .5);
-   });
+  $(".dropdown-arrow").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: $(".about").offset().top,
+      },
+      0.5
+    );
+  });
 
-  $(".rus").click(function() {
-  	$(".rus").addClass('active').removeClass('disable')
-  	$(".en").addClass('disable').removeClass('active')
-  })
+  $(".rus").click(function () {
+    $(".rus").addClass("active").removeClass("disable");
+    $(".en").addClass("disable").removeClass("active");
+  });
 
-  $(".en").click(function() {
-  	$(".en").addClass('active').removeClass('disable')
-  	$(".rus").addClass('disable').removeClass('active')
-  })
+  $(".en").click(function () {
+    $(".en").addClass("active").removeClass("disable");
+    $(".rus").addClass("disable").removeClass("active");
+  });
 
-
+  $(".nav-toogler").click(function (event) {
+    $(".nav-toogler, .nav-menu").toggleClass("active");
+    $("body").toggleClass("lock");
+  });
 });
